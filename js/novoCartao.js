763e858529a444f0b36c47f2e71ec35cdceaf55e
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-05-19 22:32:56 -0300

    Alterando metodo de exibir cartoes a partir de avaliacao com console

--- js/novoCartao.js
+++ js/novoCartao.js
@@ -3,12 +3,14 @@
 
 	$(".novoCartao").submit(function(event){
 
-		var campoConteudo = $(".novoCartao-contudo");
-
+		var campoConteudo = $(".novoCartao-conteudo");
+	
 		var conteudo = campoConteudo.val().trim()
 										  .replace(/\n/g, "<br>")
 										  .replace(/\*\*([^\*][^\*]*)\*\*/g, "<b>$1</b>")
 										  .replace(/\*([^\*]*)\*/g, "<em>$1</em>");
+
+										  console.log(campoConteudo);
 
 		if(conteudo){
 			controladorDeCartoes.adicionaCartao(conteudo);
